d158aef5daa38bceec5e6cc72b5368e6e37164e9
yavarmish <59411957+yavarmish@users.noreply.github.com>
2025-03-25 13:21:31 +0530

    Update MinRangeQuery.cpp

--- MinRangeQuery.cpp
+++ MinRangeQuery.cpp
@@ -1,21 +1,3 @@
-/*
-	Range query DS
-	Common usecases : Min/Max/Sum in a range
-
-	- Segment tree devides the entire range(root node) into subranges(each of equal size) and stores the result as node values
-	- Result is calculated in a bottom up fashion starting from indivisable ranges
-	- Results are stored in an array where each index represents level order position(assuming complete tree) of the node(a given range)
-
-	build(idx, low, high) Time O(n)
-	- Use left and right population at each level
-
-	query(idx, low, high, rangeLow, rangeHigh) Time O(log n)
-	- No overlap(don't use the result) - Partial overlap(use left and right results) - Complete overlap(use the result as it is) 
-
-	update(idx, low, high, id, val) Time O(log n)
-	- Update required index and following nodes going upwards	
-*/
-
 class SegTree {
 	vector<int> seg;
 	int n;
@@ -80,5 +62,3 @@ class SegTree {
 		update(0, 0, n - 1, id, val);
 	}
 };
-
-
